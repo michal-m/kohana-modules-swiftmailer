62f5e49df879f3b89f9ad9251859ef28cec67845
MichalM <michal.j.musial@gmail.com>
2012-10-22 13:31:32 +0200

    added SwiftMailer helper class with SwiftMailer::init() method

---
+++ classes/Helper/SwiftMailer.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * Extension of the Kohana URL helper class.
+ *
+ * @package     SwiftMailer
+ * @category    Helpers
+ * @author      Michał Musiał
+ * @copyright   (c) 2012 Michał Musiał
+ */
+class Helper_SwiftMailer
+{
+    public static function init()
+    {
+        require Kohana::find_file('vendor', 'Swift-4.2.1/lib/swift_required', 'php');
+    }
+}
---
+++ classes/SwiftMailer.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class SwiftMailer extends Helper_SwiftMailer {}
