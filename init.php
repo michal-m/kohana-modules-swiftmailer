eba453050c9ce2284cc81de920071e4b1fc24425
MichalM <michal.j.musial@gmail.com>
2012-11-29 16:03:28 +0100

    lowercasing config file to match last commit's change

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-if (Kohana::$config->load('SwiftMailer')->get('autoload'))
+if (Kohana::$config->load('swiftmailer')->get('autoload'))
 {
 	SwiftMailer::init();
 }
